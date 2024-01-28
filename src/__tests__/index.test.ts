b5660bc685ff6d1c91d445556a19c82ac87aa33a
Oliver Belmont <oliverbelmont@oliverbelmont.com>
2024-01-28 13:10:50 +1100

    math functions

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,9 +1,17 @@
-import { testFunction } from '../index'
+import { mapToRange, lerp } from '../index'
 
-describe(testFunction, () => {
-  test('return value', () => {
-    expect(testFunction({ param: 'stringParam' })).toEqual({
-      param: 'stringParam',
-    })
+describe(lerp, () => {
+  test('returns correct linear interpolation value', () => {
+    expect(lerp(0, 10, 0.5)).toEqual(5)
+  })
+
+  test('returned value will cutoff if to small', () => {
+    expect(lerp(9.99, 10, 0.2, 0.1)).toEqual(10)
+  })
+})
+
+describe(mapToRange, () => {
+  test('returns correct value mapped to new range', () => {
+    expect(mapToRange(5, 0, 10, 0, 100)).toEqual(50)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-interface Params {
-  param: string
+export const lerp = (
+  start: number,
+  end: number,
+  distance: number,
+  cutoff: number = 0
+) => {
+  const value = start + (end - start) * distance
+  // use cutoff to prevent tiny floating point numbers
+  if (end - value < cutoff) return end
+  return value
 }
 
-export const testFunction = (params: Params): Params => {
-  console.log(new Date().getMilliseconds())
-  return params
-}
+export const mapToRange = (
+  value: number,
+  oldFloor: number,
+  oldCeiling: number,
+  newFloor: number,
+  newCeling: number
+) =>
+  ((value - oldFloor) / (oldCeiling - oldFloor)) * (newCeling - newFloor) +
+  newFloor
